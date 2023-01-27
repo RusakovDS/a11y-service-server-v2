38306b8434a7b99ab1c84f8ab4a5f76531eefc6a
Dmitry Rusakov <rusakovtt@yandex.ru>
2023-01-27 17:15:20 +0600

    update CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({credentials: true, origin: ['http://localhost:5173']});
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
